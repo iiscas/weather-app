2dd11f582c85327ec3ca05537f791dfdc941422e
Isabel Castro <isabelcastro4@outlook.pt>
2024-12-17 09:21:28 +0000

    Add templates page

--- weather_app/umbrella/views.py
+++ weather_app/umbrella/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello, world. You're at the weather index.")
+    return render(request, 'homepage.html')
